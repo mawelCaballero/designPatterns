81a12badbdee624dbdbd131cf8a34148c768ff68
Manuel Caballero <info@mawel.name>
2019-01-27 14:50:25 +0100

    Adding Single responsability principle

---
+++ 1 Single responsability principle/C sharp/Agenda.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{ 
+    public class Agenda
+    {
+        private readonly List<string> entries = new List<string>();
+
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.add($"{++count} + {text}");
+            return count;
+        }
+
+        public void RemoveEntry(int index) {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+    }
+}
---
+++ 1 Single responsability principle/C sharp/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Agenda agenda = new Agenda();
+            agenda.AddEntry("Arnold es Conan");
+            agenda.AddEntry("Arnold es Dutch");
+            agenda.AddEntry("Arnold es Danko");
+            Console.Write(agenda.ToString());
+            Console.ReadKey();
+        }
+    }
+}
